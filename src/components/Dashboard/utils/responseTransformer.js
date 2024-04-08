d6855bb7f19bb11610b389c8a45103653fb30b62
shubhamchasing <shubhamchasing@gmail.com>
2024-04-08 18:19:57 +0530

    feat: Remove custom capitalize function and add css

--- src/components/Dashboard/utils/responseTransformer.js
+++ src/components/Dashboard/utils/responseTransformer.js
@@ -1,10 +1,7 @@
-import { capitalizeFirstLetter } from "../../../utils/commonFunctions";
-
 const responseTransformer = (userList) =>
   userList.map((user) => {
     return {
       ...user,
-      role: capitalizeFirstLetter(user.role),
       isSelected: false,
     };
   });
--- src/utils/commonFunctions.js
+++ src/utils/commonFunctions.js
@@ -1,9 +1,6 @@
 export const isSubstringInStringIgnoreCase = (mainString, subString) =>
   mainString.toLowerCase().includes(subString.toLowerCase());
 
-export const capitalizeFirstLetter = (stringValue) =>
-  stringValue.charAt(0).toUpperCase() + stringValue.slice(1);
-
 export const getUserDataForCurrentPage = (currentPage, pageSize, userData) => {
   const endIndex = currentPage * pageSize;
   const startIndex = endIndex - pageSize;
